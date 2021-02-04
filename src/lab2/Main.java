994878466a23bf9b135fce59f19cc1478c295676
DaddyDelicious <klasnordquist@gmail.com>
2021-02-04 13:29:47 +0100

    v0.20

--- src/lab2/Main.java
+++ src/lab2/Main.java
@@ -5,23 +5,31 @@ Assignment: Lab 2
 
 package lab2;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
+import java.io.*;
+
 
 public class Main {
     private static BufferedReader consoleReader;
     private static BufferedReader fileReader;
     private static BufferedWriter fileWriter;
 
-    public static void main(String[] args) throws Exception {
 
-        FileReader fr = new FileReader("src/Lab2/Sourcetext.txt");
+    //trash main
+    public static void main(String[] args) throws Exception{
+
+        //consoleInput(arg[0]);
+        FileReader fr = new FileReader("src/lab2/Sourcetext.txt");
         BufferedReader br = new BufferedReader(fr);
-
         String result;
+        while((result = br.readLine()) != null){
 
-        while ((result = br.readLine()) != null) {
+
+
+
+
+
+
+
             Dictionary dictionary = new Dictionary();
             dictionary.addWords(result);
             dictionary.countOccurrences();
@@ -29,9 +37,44 @@ public class Main {
         }
     }
 
-    public static String consoleInput(String arg) {
 
-        return null; //fix later//stuff //test push
+
+    public static String consoleInput(String arg) throws IOException{
+        BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+        try{
+            System.out.println("Select option: ");
+            String line = consoleReader.readLine();
+            switch (line){
+                case "end":
+                    System.out.println("Bye.");
+                    break;
+                case "1":
+                    System.out.println("Load from file");
+                    System.out.println("File name: ");
+                    String choice = consoleReader.readLine();
+                    if(!choice.equals("Sourcetext.txt")){
+                        System.out.println("File does not exist!");
+                    }
+                    else{
+                        FileReader fr = new FileReader("src/lab2/Sourcetext.txt");
+                        BufferedReader br = new BufferedReader(fr);
+                        String result;
+                        while((result = br.readLine()) != null){
+                            System.out.println(result);
+                        }
+                    }
+                case "2":
+                    System.out.println("List content");
+                    System.out.println("Output format (0 or 1): ");
+                    int choice2 = consoleReader.read();
+                    Word.changeOutputFormat(choice2);
+
+            }
+
+        } catch (IOException e) {
+            return "";
+        }
+
+        return null;
     }
-
 }
